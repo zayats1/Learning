2ebb17e102dd3e411803a8e5e86d1632e6434f45
slavik <zaiats.bogdan@gmail.com>
2022-04-12 13:50:59 +0300

    Lab4 done

--- src/lab4/Lab4.java
+++ src/lab4/Lab4.java
@@ -1,7 +1,29 @@
 package lab4;
+import static java.lang.System.*;
 
 public class Lab4 {
-    public static void main(String[] args) {
+     static final int Min = 3;
+     static final int Max = 7;
 
+    public static void main(String[] args) {
+         out.println("Min = " + Min + " Max = " + Max);
+         out.println("Sum = " + sum_for(Min,Max));
+         out.println("Product = " + product_for(Min,Max));
+    }
+
+    static int sum_for(int min, int max) {
+        int sum = 0;
+        for (int num = min; num <= max; num++) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    static int product_for(int min, int max) {
+        int prod = 1;
+        for (int num = min; num <= max; num++) {
+            prod *= num;
+        }
+        return prod;
     }
 }
---
+++ src/lab5/Lab5.java
@@ -0,0 +1,7 @@
+package lab5;
+
+public class Lab5 {
+    public static void main(String[] args) {
+
+    }
+}
