4503a4f9237614085443800c9dccf9811f9e3624
slavik <zaiats.bogdan@gmail.com>
2022-04-13 22:06:00 +0300

    Lab 6 done

--- src/lab6/Lab6.java
+++ src/lab6/Lab6.java
@@ -1,4 +1,24 @@
 package lab6;
 
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
 public class Lab6 {
+    static  int[] numbers = {0,1,2,3,4,5,6,7};
+    static final  int MULTIPLIER = 2;
+
+    public static void main(String[] args) {
+        out.println(Arrays.toString(multiplyArrayValues(numbers, MULTIPLIER)));
+        out.println(Arrays.toString(numbers));
+    }
+
+    static int [] multiplyArrayValues(int [] array,int multiplier){
+        int [] arr = array.clone(); // otherwise, array will be mutated
+        for (int i =0; i < arr.length; i++) {
+            arr[i] *= multiplier;
+        }
+        return  arr;
+    }
+
 }
