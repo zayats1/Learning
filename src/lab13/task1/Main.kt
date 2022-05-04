f28b297fd1b1541d0a317a8357913a96aa74753e
zayats1 <zaiats.bogdan@gmail.com>
2022-05-04 14:43:12 +0300

    refactor code

--- src/lab13/task1/Main.kt
+++ src/lab13/task1/Main.kt
@@ -1,4 +1,4 @@
-package lab13
+package lab13.task1
 
 import javax.swing.JButton
 import javax.swing.JFrame
@@ -11,13 +11,12 @@ fun main() {
     val programFrame = JFrame("MyProgram")
     programFrame.setSize(HEIGHT, WIDTH)
     programFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-
     programFrame.layout = null      // I don`t use any layout managers in this program
+    programFrame.isVisible = true
 
     val myButton = JButton("Butt")
-
     myButton.setBounds(130, 100, 100, 40);
-    programFrame.layout = null;
-    programFrame.isVisible = true
+
+
     programFrame.add(myButton)
 }
