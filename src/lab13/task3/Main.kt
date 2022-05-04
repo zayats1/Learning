65b9129f1154a3a87d3803ff563397705d41914d
zayats1 <zaiats.bogdan@gmail.com>
2022-05-04 15:43:27 +0300

    fix

---
+++ src/lab13/task3/Main.kt
@@ -0,0 +1,35 @@
+package lab13.task3
+
+import java.awt.BorderLayout
+import java.awt.BorderLayout.*
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+
+
+class MyFrame
+    () : JFrame() {
+    init {
+        title = "Task2"
+        layout = BorderLayout()
+        isVisible = true
+
+        val myButtons = Array<JButton>(5) { i -> JButton("Button$i") }
+        for (butt in myButtons) {
+            butt.preferredSize = Dimension(100, 100)
+        }
+
+        add(myButtons[0], NORTH)
+        add(myButtons[1], WEST)
+        add(myButtons[2], CENTER)
+        add(myButtons[3], EAST)
+        add(myButtons[4], SOUTH)
+
+
+        setSize(500, 400)
+    }
+}
+
+fun main() {
+    val frame = MyFrame()
+}
---
+++ src/lab13/task4/Main.kt
@@ -0,0 +1,5 @@
+package lab13.task4
+
+fun main() {
+
+}
